276c7c218dc032d3f604af6cbbbec1cb62becd01
Cole Allan <cacoleallan@gmail.com>
2017-04-25 15:21:10 -0600

    Finished assessment

--- app.js
+++ app.js
@@ -1,25 +1,32 @@
 angular.module("assessment", ['ui.router'])
-.config(function($stateProvider, $urlRouterProvider){
+  .config(function($stateProvider, $urlRouterProvider){
+    $urlRouterProvider.when('', '/')
     $urlRouterProvider.otherwise('/')
+
     $stateProvider
-    .state('home', {
+      .state('home', {
+        // controller: 'shopCtrl',
         url: '/',
         templateUrl: './views/home.html'
-    })
-
-    .state('about', {
+      })
+      .state('about', {
+        // controller: 'shopCtrl',
         url: '/about',
         templateUrl: './views/about.html'
-    })
-
-    .state('shop', {
-        url: '/shop',
-        templateUrl: './views/shop.html',
-        controller: 'shopCtrl'
-    })
-
-    .state('blog', {
+      })
+      .state('blog', {
+        // controller: 'shopCtrl',
         url: '/blog',
         templateUrl: './views/blog.html'
-    })
-})
+      })
+      .state('shop', {
+        controller: 'shopCtrl',
+        url: '/shop',
+        templateUrl: './views/shop.html'
+      })
+      .state('details', {
+        controller: 'productDetailCtrl',
+        url: '/details/:id',
+        templateUrl: './views/product-details.html'
+      })
+  })
